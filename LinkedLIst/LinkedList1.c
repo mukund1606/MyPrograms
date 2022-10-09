df8fdd67a446ad376693b87217e06fa7e7964bdb
Mukund Mittal <mukund.mukulmittal@gmail.com>
2022-10-09 20:51:17 +0530

    Adding in B/w Two Nodes

--- LinkedLIst/LinkedList1.c
+++ LinkedLIst/LinkedList1.c
@@ -23,7 +23,7 @@ void push(struct Node** head_ref, int new_data){
 
 
 //Adding Node to end
-void add(struct Node** node, int new_data){
+void add_end(struct Node** node, int new_data){
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
     struct Node* temp = *node;
     while(temp->next != NULL){
@@ -32,6 +32,30 @@ void add(struct Node** node, int new_data){
     new_node->data = new_data;
     new_node->next = NULL;
     (temp)->next = new_node;
+}
+
+//Adding Node in Between
+void add(struct Node** node, int new_data){
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    struct Node* temp = *node;
+    struct Node* prev;
+    while(temp->data < new_data && temp->next != NULL){
+        prev = temp;
+        temp = temp->next;
+    }
+    if(temp == *node){
+        new_node->next = *node;
+        *node = new_node;
+    }
+    else if(temp->next == NULL){
+        new_node->next = NULL;
+        temp->next = new_node;
+    }
+    else{
+        new_node->next = temp;
+        prev->next = new_node;
+    }
 }
 
 int main(){
@@ -44,7 +68,7 @@ int main(){
     third = (struct Node*)malloc(sizeof(struct Node));
 
 
-    head->data = 1;
+    head->data = 2;
     head-> next = second;
 
     second->data = 2;
@@ -54,9 +78,11 @@ int main(){
     third->next = NULL;
 
     // push(&head, 23);
-    add(&head, 5);
-    add(&second, 6);
-    add(&head, 8);
+    add_end(&head, 5);
+    add_end(&head, 9);
+    add_end(&head, 8);
+    add(&head, 1);
+    add(&head, 11);
     // printf("%d", head);
     printList(head);
 
